9ac4514fc861645f17ff0283861f27880b6ad30d
AdamBort <bortkun62@gmail.com>
2022-05-16 11:53:38 +0200

    created service

---
+++ Library-project/src/app/Services/service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IBoook } from '../Interfaces/IBook';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibraryService {
+  private eventsPath = 'library';
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(){
+    return this.http.get<IBoook[]>(environment.apiUrl+this.eventsPath);
+  }
+}
