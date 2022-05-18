802dc7e73fada3457a0df4db51e7d9621c5f43c6
AdamBort <bortkun62@gmail.com>
2022-05-18 12:30:58 +0200

    Handled deleting books and changing status + added new background

--- Library-project/src/app/app.component.ts
+++ Library-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import { libraryService } from './Services/libraryservice';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './Components/modal/modal.component';
+import Swal from 'sweetalert2'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
@@ -25,5 +26,54 @@ export class AppComponent implements OnInit{
 
   openModal() {
     const modalRef = this.modalService.open(ModalComponent)
+  }
+
+  deleteBook(id: any) {
+    Swal.fire({
+      title: 'Do you want to delete this book?',
+      showDenyButton: true,
+      confirmButtonText: 'Delete',
+      denyButtonText: `Cancel`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Book has been deleted',
+          timer: 2000,
+          showConfirmButton: false
+      })
+      axios.delete(`http://localhost:1337/api/books/${id}`)
+      setTimeout(function(){
+        window.location.reload()
+      }, 2000)
+      }
+    })
+  }
+
+  changeStatusBook(id: any, status: boolean) {
+    if (status == true){
+      axios.put(`http://localhost:1337/api/books/${id}`, {
+        data: {
+          borrowed: false
+        }
+      })
+    }
+    else {
+      axios.put(`http://localhost:1337/api/books/${id}`, {
+        data: {
+          borrowed: true
+        }
+      })
+    }
+    Swal.fire({
+      icon: 'success',
+      title: "The book's status has changed",
+      timer: 1500,
+      showConfirmButton: false
+  })
+  setTimeout(function(){
+    window.location.reload()
+  }, 1600)
   }
 }
